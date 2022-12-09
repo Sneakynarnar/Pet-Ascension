2478c795c354e606e89e6f0c5051bd6296a0271d
Sneakynarnar <77871782+EliteNarNar@users.noreply.github.com>
2022-12-09 01:23:37 +0000

    Static files are now served and input verification

--- client-side/index.js
+++ client-side/index.js
@@ -2,20 +2,27 @@
 
 function inputHandler(e){
     const inputBox = document.querySelector("#petname")
-    const name = inputBox.value.toLowerCase()
+    const name = inputBox.value
     let allowedCharacters = "qwertyuiopasdfghjklzxcvbnm-_1234567890"
     console.log(name)
-    
-    for (char of name){
-        if (!!allowedCharacters.includes(char)){
-            inputBox.classList.add("invalid")
-            
-            
-        } else if(inputBox.classList.contains("invalid")){
-            inputBox.classList.remove("invalid")
+    let invalid = false
+    for (const char of name){
+        if (!allowedCharacters.includes(char)){
+            inputBox.classList.add("invalid") 
+            invalid = true
+            break
         }
+
         
     }
+    if(!invalid && inputBox.classList.contains("invalid")){
+            
+            
+        inputBox.classList.remove("invalid")
+    
+    }
+        
+    
     
 
 }
--- server-side/app.js
+++ server-side/app.js
@@ -1,85 +1,80 @@
-const express = require("express")
-const path = require("path")
-const fs = require("fs/promises")
-const open = require("open")
-const app = express()
-const PORT = 8080
+const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const open = require("open");
+const app = express();
+const PORT = 8080;
 let data = {
     hunger: 500,
     level: 2,
     entertainment: 3    
-}
+};
 
-app.use(express.static("./client-side/homepage"));
-app.use(express.urlencoded({extended: false}))
-app.get("/", (req, res) => {
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/homepage/index.html"));
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/homepage/style.css"));
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/homepage/index.js"));
-    }   
-)
+//app.use(express.static("./client-side/homepage"));
+app.use(express.urlencoded({extended: false}));
+app.use("/",express.static(path.join(__dirname, "../client-side")));
+//app.use("/pets", express.static(path.join(__dirname, "client-side/pets")))
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/homepage/index.html"));
+// })
 
 
-app.post("/createpet",  async (req, res) =>{
-    let data = await fs.readFile("server-side/pets.json")
-    let pets = JSON.parse(data)   
+app.post("/createpet", async (req, res) => {
+    let data = await fs.readFile("server-side/pets.json");
+    let pets = JSON.parse(data);   
     if (pets[req.body.petname] ===undefined){
-        pets[req.body.petname] = {
+        pets[req.body.petname.toLowerCase()] = {
                 type: req.body.antype,
                 cleanliness: 50,
                 hunger: 50,
                 level: 1,
                 rank: 1,
         }
-        res.set("Content-Type", "application/json");
-        res.send(JSON.stringify(pets));
+   
+        
+    fs.writeFile("server-side/pets.json", JSON.stringify(pets));
+    res.redirect("http://localhost:8080/pets")
 
-        fs.writeFile("server-side/pets.json", JSON.stringify(pets))
     } else {
-
-        res.send("A pet already exists with that name!")
+        res.send("A pet already exists with that name!");
     }
     
 
 })
 
 app.get("/pets", async (req, res) => {
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/pets/index.html"));
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/pets/style.css"));
-    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/pets/index.js"));
-    //res.sendFile(path.join(path.resolve(__dirname, ".."), "/server-side/pets.json"));
-    
+    res.sendFile(path.join(path.resolve(__dirname, ".."), "/client-side/pets/index.html"));   
     let data = await fs.readFile("server-side/pets.json")
     let pets = JSON.parse(data)   
     let petName = req.baseUrl.slice(5)
-    // if (pets[petName] !== undefined){
-    //     res.status(404).send("Bad Request")
-
-    // }
     
 })
 
-app.get("/pets/*", async (req, res) => {
+app.get("/pets/:petName", async (req, res) => {
     let data = await fs.readFile("server-side/pets.json")
     let pets = JSON.parse(data)     
-    let petName = req.baseUrl.slice(5)
-    res.send(petName)
-    // if (pets[petName] !== undefined){
-    //     res.status(404).send("Bad Request")
+    let petName = req.params["petName"]
 
-    // }
     
+    if (pets[petName] === undefined){
+        res.status(404).end("Not found" + JSON.stringify(pets))
+        return
+    }
+    res.send(petName)
 })
 
-// app.get("/api", (req, res) => {
+app.get("/api", async (req, res) => {
 
-//     res.set("Content-Type", "application/json");
-//     res.send(JSON.stringify(data));
-// })
+    res.set("Content-Type", "application/json");
+    let data = await fs.readFile("server-side/pets.json")
+    let pets = JSON.parse(data)    
+    res.send(JSON.stringify(data));
+})
 
 app.listen(PORT, (req, res) => {
-    
+    //open("http://localhost:8080")
     console.log(`Listening on port ${PORT}`);
+    
 
     
 })
